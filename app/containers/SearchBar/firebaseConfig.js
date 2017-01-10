ad6900e6cd4dfc2e3e2de5a6cd1b3e00d111fabd
RichAbrahams <richardabrahams75@gmail.com>
2017-01-10 13:48:31 +0000

    fb config

--- app/containers/SearchBar/firebaseConfig.js
+++ app/containers/SearchBar/firebaseConfig.js
@@ -1,7 +1,9 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: 'AIzaSyAJLi0zEa9viBHyXLcoOXm6gmj7JsALve4',
   authDomain: 'silent-wharf-153101.firebaseapp.com',
   databaseURL: 'https://silent-wharf-153101.firebaseio.com',
   storageBucket: 'silent-wharf-153101.appspot.com',
   messagingSenderId: '411725484863',
 };
+
+export default firebaseConfig;
