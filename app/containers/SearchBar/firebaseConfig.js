f48a8bfcc85d5a4bbb1e0e4d114a8da810a5ee44
RichAbrahams <richardabrahams75@gmail.com>
2017-01-10 13:09:13 +0000

    new commit

---
+++ app/containers/SearchBar/firebaseConfig.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAJLi0zEa9viBHyXLcoOXm6gmj7JsALve4',
+  authDomain: 'silent-wharf-153101.firebaseapp.com',
+  databaseURL: 'https://silent-wharf-153101.firebaseio.com',
+  storageBucket: 'silent-wharf-153101.appspot.com',
+  messagingSenderId: '411725484863',
+};
